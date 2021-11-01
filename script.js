dbd817daa12b314726543753a1f4ddaf4f5cf769
emorales <emorales@kdoce.cl>
2021-11-01 18:39:27 -0300

    adding documentation

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const divsDataKeys = document.querySelectorAll(".key");
 // remove classList firts
 divsDataKeys.forEach(element => element.addEventListener('transitionend',removeTransition));
 
+/**
+ * Play Key Sound: just play sound by key press.
+ *
+ * @param {Event} e event to raised.
+ * @return {void} 
+ */
 function playKeySound(e){
     const keyCode = e.keyCode;
     const divDataKey = document.querySelector(`[data-key= '${keyCode}']`);
     
-    //console.log(divDataKey);
     if(!divDataKey) return;
-    
     // add class
     divDataKey.classList.add('playing');
     // play sound
@@ -20,6 +24,12 @@ function playKeySound(e){
    audio.play();
 }
 
+/**
+ * Remove Transition: just remove class playing by key press.
+ *
+ * @param {Event} e event to raised.
+ * @return {void} 
+ */
 function removeTransition(e){
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
